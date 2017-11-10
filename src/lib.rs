c2555ff8976d1d46d6bb9d62112851df590bf5be
Peter Williams <pwil3058@gmail.com>
2017-11-10 11:16:04 +1000

    move: move source files into src directory
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+// Copyright 2017 Peter Williams <pwil3058@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub mod characteristics;
+pub mod colour_mix;
+pub mod components;
+pub mod hue_wheel;
+pub mod mixed;
+pub mod mixer;
+pub mod model_paint;
+pub mod paint;
+pub mod series;
+pub mod shape;
+pub mod target;
+
 #[cfg(test)]
 mod tests {
     #[test]
